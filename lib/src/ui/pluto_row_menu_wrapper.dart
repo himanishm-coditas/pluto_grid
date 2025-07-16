e7b15e6cd9f23c56383e15ed22ad10c26baaad3e
Apoorv soni <apoorv.soni@coditas.com>
2025-07-16 14:07:55 +0530

    fix:bug on dragging row and releasing due to not closing of menuanchor

--- lib/src/ui/pluto_row_menu_wrapper.dart
+++ lib/src/ui/pluto_row_menu_wrapper.dart
@@ -26,91 +26,56 @@ class PlutoRowMenuWrapper extends StatefulWidget {
 class _PlutoRowMenuWrapperState extends State<PlutoRowMenuWrapper> {
   final _menuController = MenuController();
   final _optionsMenuController = MenuController();
+
   bool _isInteractingWithMenu = false;
   bool _isDisposed = false;
-  bool _isClosingMenu = false;
 
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((final _) {
-      if (!_isDisposed && mounted) {
-        widget.stateManager.addListener(_handleRowHoverChange);
-      }
-    });
+    widget.stateManager.addListener(_handleRowHoverChange);
   }
 
   @override
   void dispose() {
     _isDisposed = true;
     widget.stateManager.removeListener(_handleRowHoverChange);
-    _closeMenuSafely(_menuController);
-    _closeMenuSafely(_optionsMenuController);
     super.dispose();
   }
 
   void _handleRowHoverChange() {
-    if (_isDisposed || !mounted || _isClosingMenu) return;
+    if (_isDisposed || !mounted || widget.stateManager.isDraggingRow) return;
+
 
     final isHovered = widget.stateManager.isRowHovered(widget.row);
 
     if (!isHovered) {
-      _closeMenuSafely(_optionsMenuController);
-      _closeMenuSafely(_menuController);
+      if (_menuController.isOpen) _menuController.close();
+      if (_optionsMenuController.isOpen) _optionsMenuController.close();
     } else if (isHovered && !_menuController.isOpen) {
-      _openMenuSafely(_menuController);
+      _menuController.open();
     }
   }
 
-  void _closeMenuSafely(final MenuController controller) {
-    if (_isDisposed || !mounted || _isClosingMenu || !controller.isOpen) return;
-
-    _isClosingMenu = true;
-    WidgetsBinding.instance.addPostFrameCallback((final _) {
-      if (!_isDisposed && mounted) {
-        try {
-          controller.close();
-        } catch (e) {
-          debugPrint('Error closing menu: $e');
-        } finally {
-          _isClosingMenu = false;
-        }
-      }
-    });
-  }
-
-  void _openMenuSafely(final MenuController controller) {
-    if (_isDisposed || !mounted || controller.isOpen) return;
-
-    WidgetsBinding.instance.addPostFrameCallback((final _) {
-      if (!_isDisposed && mounted) {
-        try {
-          controller.open();
-        } catch (e) {
-          debugPrint('Error opening menu: $e');
-        }
-      }
-    });
-  }
-
   @override
-  Widget build(final BuildContext context) => MenuAnchor(
+  Widget build(BuildContext context) {
+    return MenuAnchor(
       controller: _menuController,
       alignmentOffset: widget.menuOffset,
       onOpen: () => _isInteractingWithMenu = true,
       onClose: () {
         _isInteractingWithMenu = false;
         if (!widget.stateManager.isRowHovered(widget.row)) {
-          _closeMenuSafely(_menuController);
+          widget.stateManager.clearHoveredRow();
         }
       },
       menuChildren: [
         MouseRegion(
-          onEnter: (final _) {
+          onEnter: (_) {
             widget.stateManager.clearHoveredRow();
             widget.stateManager.setHoveredRow(widget.row);
           },
-          onExit: (final _) {
+          onExit: (_) {
             if (!_isInteractingWithMenu) {
               widget.stateManager.clearHoveredRow();
             }
@@ -119,9 +84,8 @@ class _PlutoRowMenuWrapperState extends State<PlutoRowMenuWrapper> {
             mainAxisSize: MainAxisSize.min,
             children: [
               if (widget.menuChildren?.isNotEmpty ?? false)
-                ...widget.menuChildren!
-                    .map(
-                      (final menuItem) => Padding(
+                ...widget.menuChildren!.map(
+                      (menuItem) => Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 8.0),
                     child: menuItem,
                   ),
@@ -132,12 +96,14 @@ class _PlutoRowMenuWrapperState extends State<PlutoRowMenuWrapper> {
                   crossAxisUnconstrained: false,
                   onOpen: () {
                     _isInteractingWithMenu = true;
-                    _openMenuSafely(_menuController);
+                    if (!_menuController.isOpen) {
+                      _menuController.open();
+                    }
                   },
                   onClose: () {
                     _isInteractingWithMenu = false;
                     if (!widget.stateManager.isRowHovered(widget.row)) {
-                      _closeMenuSafely(_menuController);
+                      widget.stateManager.clearHoveredRow();
                     }
                   },
                   menuChildren: widget.menuOptions!,
@@ -146,9 +112,9 @@ class _PlutoRowMenuWrapperState extends State<PlutoRowMenuWrapper> {
                     child: InkWell(
                       onTap: () {
                         if (_optionsMenuController.isOpen) {
-                          _closeMenuSafely(_optionsMenuController);
+                          _optionsMenuController.close();
                         } else {
-                          _openMenuSafely(_optionsMenuController);
+                          _optionsMenuController.open();
                         }
                       },
                       child: const Icon(Icons.more_vert),
@@ -161,4 +127,5 @@ class _PlutoRowMenuWrapperState extends State<PlutoRowMenuWrapper> {
       ],
       child: widget.dragTarget,
     );
+  }
 }
